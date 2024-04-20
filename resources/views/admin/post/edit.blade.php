94034d2a09164398ee4c1b96edaa20c0c2deef63
Mohsen Safari <safari.tafreshi@gmail.com>
2024-04-20 11:14:57 +0330

    now edit post shows uploaded images in dropbox

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -34,11 +34,42 @@
 
 <div class="mt-5">
     <p class="mb-3"> {{ __('Drop files here for upload.') }} </p>
-    <form action="{{ route('admin.posts.upload_files', ['post' =>$row]) }}" class="dropzone">@csrf</form>
+    <form action="{{ route('admin.posts.upload_files', ['post' =>$row]) }}" class="dropzone" id="dropzone">@csrf</form>
 </div>
 
 
 <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
 <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
 
+<style>
+.dropzone .dz-image img {
+    width: 120px;
+    height: 120px;
+}
+</style>
+
+<script>
+Dropzone.options.dropzone = {  
+    init: function () {
+        var mockFile;
+        
+        @foreach ($row->files as $file)
+            mockFile = { 
+                name: '{{ asset($file->path) }}',
+                type: 'image/jpeg',
+                url: '{{ asset('/storage/' . $file->path) }}',
+                status: this.ADDED,
+                accepted: true,
+                //size: 100000
+            };
+                
+            this.emit("addedfile", mockFile);
+            this.emit("thumbnail", mockFile, '{{ asset("/storage/" . $file->path) }}');
+            this.emit("complete", mockFile);
+            this.files.push(mockFile);
+        @endforeach
+    }
+};
+</script>
 @endsection
